e39de6c8f2c48445f0d425ac34d9b7f21a2c6ad9
CarManBelarus <24219338+CarManBelarus@users.noreply.github.com>
2026-03-25 15:40:18 +0300

    Update config.js
    Ліквідаваны MUSIXMATCH_API_KEY, паколькі бібліятэка goofy 3.0.0 больш не выкарыстоўвае гэты сэрвіс, што дазваляе пазбавіцца ад лішніх налад. Паніжэнне REQUESTS_IN_ROW да 10 з'яўляецца крытычным патрабаваннем для стабільнай працы CustomUrlFetchApp, якое гарантуе, што жорсткія ліміты хуткасці Spotify API не будуць перавышаны падчас масавага збору даных.

--- config.js
+++ config.js
@@ -6,7 +6,6 @@ function setProperties() {
     UserProperties.setProperty('PRIVATE_CLIENT_SECRET', 'вашаЗначэнне');
 
     UserProperties.setProperty('LASTFM_API_KEY', 'вашаЗначэнне');
-    UserProperties.setProperty('MUSIXMATCH_API_KEY', 'вашаЗначэнне');
 
     UserProperties.setProperty('ON_SPOTIFY_RECENT_TRACKS', 'true');
     UserProperties.setProperty('ON_LASTFM_RECENT_TRACKS', 'false');
@@ -17,17 +16,19 @@ function setProperties() {
 
     UserProperties.setProperty('LOG_LEVEL', 'info');
     UserProperties.setProperty('LOCALE', 'RU'); // Лакаль для адказаў ад Spotify (напрыклад, назвы катэгорый)
-    UserProperties.setProperty('REQUESTS_IN_ROW', '20');
+    
+    // ЗМЯНЕННЕ 3.0.0: Ліміт запытаў строга 10 для стабільнасці
+    UserProperties.setProperty('REQUESTS_IN_ROW', '10');
     UserProperties.setProperty('MIN_DICE_RATING', '0.6005');
 }
 
 // Каб паглядзець бягучыя значэнні параметраў
-// function logProperties() {
-//     console.log(UserProperties.getProperties())
-// }
+function logProperties() {
+    console.log(UserProperties.getProperties());
+}
 
 // Каб скінуць аўтарызацыю і параметры
-// function reset() {
-//     Admin.reset()
-//     setProperties()
-// }
+function reset() {
+    Admin.reset();
+    setProperties();
+}
